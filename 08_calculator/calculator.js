ed855ba40533082b97c5fa6bbb36ba74731504aa
Giovanni Torelli <26353442+Torelli@users.noreply.github.com>
2023-01-31 19:36:09 -0300

    Exercise 8 solved!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, number) => sum + number,0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((product, number) => product * number,1);
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let number = n;
+  let numbers = [];
+	if(n === 0) {
+    return 1;
+  }
+  for(let i = 0; i < n; i++) {
+    numbers.push(number);
+    number --;
+  }
+  return numbers.reduce((acc,number) => acc * number,1);
 };
+
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
