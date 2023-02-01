528b651ea144b9f2556ac02a1a0d829613e68e09
Giovanni Torelli <26353442+Torelli@users.noreply.github.com>
2023-02-01 00:28:04 -0300

    Exercise 9 solved!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.replaceAll(/[!.,\s]/g,"").toLowerCase();
+    let revString = str.replaceAll(/[!.,\s]/g,"").toLowerCase().split("").reverse().join("");
+    return string === revString;
 };
 
 // Do not edit below this line
