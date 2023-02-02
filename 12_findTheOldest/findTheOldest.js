80db161e3adb3026fd2ccf3ffdd0ccd569fd11f9
Giovanni Torelli <26353442+Torelli@users.noreply.github.com>
2023-02-01 21:42:34 -0300

    Exercise 12 solved!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+        if(!oldestPerson.yearOfBirth) oldestPerson.yearOfBirth = ((new Date()).getFullYear());
+        if(!oldestPerson.yearOfDeath) oldestPerson.yearOfDeath = ((new Date()).getFullYear());
+        if(!currentPerson.yearOfDeath) currentPerson.yearOfDeath = ((new Date()).getFullYear());
+        let currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        let oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+    }, {});
 };
 
 // Do not edit below this line
