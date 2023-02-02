1b4194d49bcfa36db86fdf0645b51acf9d958903
Giovanni Torelli <26353442+Torelli@users.noreply.github.com>
2023-02-01 21:00:00 -0300

    Exercise 10 solved!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) return "OOPS";
+    member = member - 1;
+    const sequence = [1,1];
+    let n1;
+    let n2;
+    for(let i = 0; i < member; i++) {
+        n1 = sequence[sequence.length - 2];
+        n2 = sequence[sequence.length - 1];
+        sequence.push(n1 + n2);
+    }
+    return sequence[member];
 };
-
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
